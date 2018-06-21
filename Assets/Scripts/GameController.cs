0fd571257d885ab9cd059ca723f513409b1358cc
Aleksei Palma <supersonic616@hotmail.com>
2018-06-21 15:22:09 -0500

    Trabajo unity

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿//controlador
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	
+	public float Speed = 11f;
+	public static GameController instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DeAccel()
+	{
+		while(Speed > 0)
+		{
+			Speed -= 0.01f;
+		}
+	} 
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿//script de deteccion de objetos
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// movimiento del jugador 
+    // movimiento de derecha a izquierda
+	void Update () {
+
+		Target.transform.position = new Vector3(Target.transform.position.x, Target.transform.position.y, transform.position.z);
+
+		transform.position = Vector3.MoveTowards(transform.position,Target.position, 10 * Time.deltaTime);
+		
+	}
+
+    // interaccion con los bloques
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Plantilla")
+		{
+			//Generator.instance.Spawn();
+			Destroy(other.gameObject,1.5f);	
+		}
+		
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Cobweb")
+		{
+			GameController.instance.Speed = 0;
+			StopCoroutine("Restart");
+			StartCoroutine("Restart");
+		}
+
+		if(other.gameObject.tag == "Coin")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Hazard")
+		{
+			GameController.instance.InvokeRepeating("DeAccel",0.1f,2);
+			StopCoroutine("Restart");
+			StartCoroutine("Restart");			
+		}
+	}
+
+    // reinicio de nivel
+	private IEnumerator Restart()
+	{
+		yield return new WaitForSeconds(1.5f);
+		SceneManager.LoadScene(0);
+	} 
+}
